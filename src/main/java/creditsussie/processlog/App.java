78cf7790e2ad073ec1ef274f582c1b3fdb4ab944
meerac77 <112803388+meerac77@users.noreply.github.com>
2022-09-04 12:46:50 +0200

    Updated App.java

--- src/main/java/creditsussie/processlog/App.java
+++ src/main/java/creditsussie/processlog/App.java
@@ -13,11 +13,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class App {
+
+	public static final Logger logger = LoggerFactory.getLogger(App.class);
+
 	public static void main(String[] args) throws InterruptedException {
-
-		final Logger logger = LoggerFactory.getLogger(App.class);
-
-		logger.info("Processing Starts...");
 
 		if (args.length < 1) {
 			logger.error("Please include the path of the log file as command line argument");
@@ -25,13 +24,27 @@ public class App {
 		}
 
 		String pathName = args[0];
-
 		pathName = pathName.trim();
 		if (null == pathName || pathName.length() == 0) {
 			logger.error("Please enter a valid value for the path of the log file");
 			System.exit(1);
 		}
 
+		boolean status = startProcessing(pathName);
+		if(status) {
+			logger.info("Log file processed successfully!");
+		} else {
+			logger.error("Error encountered while processing log file!");
+		}
+
+	}
+
+	public static boolean startProcessing(String pathName) {
+
+		logger.info("Processing Starts...");
+
+		boolean status = false;
+		
 		File file = new File(pathName);
 		long fileLength = file.length();
 
@@ -52,26 +65,26 @@ public class App {
 			}
 			logger.info("File reading ends!");
 			logger.debug("Number of log lines read from file = " + lines.size());
-			
+
 			LogProcessor lProcessor = new LogProcessor(lines);
 			ForkJoinPool lThreadPool = new ForkJoinPool();
 			lThreadPool.execute(lProcessor);
 			lThreadPool.shutdown();
 			List<LogObject> logList = lProcessor.join();
 			logger.debug("Number of log lines processed = " + logList.size());
-			
+
 			LogObjectProcessor lObjProcessor = new LogObjectProcessor(logList);
 			ForkJoinPool pThreadPool = new ForkJoinPool();
 			pThreadPool.execute(lObjProcessor);
 			pThreadPool.shutdown();
 			List<ProcessedLogObject> results = lObjProcessor.join();
 			logger.debug("Number of records to be inserted = " + results.size());
-			
+
 			DBOperations db = new DBOperations();
-			db.writeToDB(results);
-			
+			status = db.writeToDB(results);
+
 		} catch (FileNotFoundException e) {
-			logger.error("FileNotFoundException: " + e.getStackTrace()); 
+			logger.error("FileNotFoundException: " + e.getStackTrace());
 		} catch (IOException e) {
 			logger.error("IOException: " + e.getStackTrace());
 		} finally {
@@ -84,12 +97,7 @@ public class App {
 			}
 		}
 		
-		//ProcessFile processor = new ProcessFile();
-		//processor.processLogFile(file);
-		//List<LogObject> logObjList = processor.processLogFile(file);
-		//System.out.println("Size = " + logObjList.size());
-
 		logger.info("Processing Ends!");
-
+		return status;
 	}
 }
